2ad35197a81f473751434f998142493d3b060fbd
Alexandre Bekor <contato@alexandrebekor.com>
2021-11-18 15:39:18 -0300

    Images Up

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.css'
+
+const Header = () => {
+    return (
+        <div>
+            <p className="paragraph">Parágrafo</p>
+        </div>
+    )
+}
+
+export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,36 @@
 import React from 'react'
+import { graphql } from 'gatsby'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+import Header from '../components/Header'
+import '../assets/style.css'
 
-const Index = () => {
+const Index = ({ data }) => {
     return (
         <div>
-            <h1>Hello World</h1>
+            <Header />
+            <p>Testando</p>
+            {data.allFile.edges.map(item => {
+                const image = getImage(item.node)
+                return (
+                    <GatsbyImage image={image} />
+                )
+            })}
         </div>
     )
 }
+
+export const pageContent = graphql`
+query MyQuery {
+    allFile(filter: { sourceInstanceName: { eq: "images" } }) {
+    edges {
+      node {
+        childImageSharp {
+          gatsbyImageData
+        }
+      }
+    }
+  }
+}
+`
 
 export default Index
